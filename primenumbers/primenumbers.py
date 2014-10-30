ee7abcb9550386d4c50379a44ab881f63a15c27d
Greg <richar45@students.rowan.edu>
2014-10-30 10:43:29 -0400

    Update primenumbers.py
    added comments

--- primenumbers/primenumbers.py
+++ primenumbers/primenumbers.py
@@ -1,16 +1,17 @@
 __author__ = 'greg'
-
+# prompt the user for the number we are going to use and return that number
 def get_input():
     number = int(input("Enter a number: "))
     return number
 
-
+# loops through all numbers up to an including the one the user entered and passes each one as a parameter for is_prime(num)
+# and print only the ones that are found to be prime
 def get_primes(n):
     for num in range(2, n + 1):
         if is_prime(num):
             print(num)
 
-
+# determines if the number passed as a parameter is prime or not; returns true if the given number is prime, false otherwise
 def is_prime(num):
     prime = True
     for x in range(2, num):
