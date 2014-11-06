15a5ae052005b4557ae3e9e0fc1f5e990fe78e38
Greg <richar45@students.rowan.edu>
2014-11-05 19:01:01 -0500

    Update primenumbers.py

--- primenumbers/primenumbers.py
+++ primenumbers/primenumbers.py
@@ -1,23 +1,35 @@
 __author__ = 'greg'
-# prompt the user for the number we are going to use and return that number
-def get_input():
+
+
+# prompt the user for the number we are going to use 
+def main():
+    primenumbers = Prime()
     number = int(input("Enter a number: "))
-    return number
+    primenumbers.get_primes(number)
+    
 
-# loops through all numbers up to an including the one the user entered and passes each one as a parameter for is_prime(num)
-# and print only the ones that are found to be prime
-def get_primes(n):
-    for num in range(2, n + 1):
-        if is_prime(num):
-            print(num)
+class Prime:
+    ## this class finds all of the prime numbers up to and including the number entered by the uer
+    
+    def __init__(self):
+        print("")
+    
+    
+    # loops through all numbers up to an including the one the user entered and passes each one as a parameter for is_prime(num)
+    # and print only the ones that are found to be prime
+    def get_primes(self, n):
+        for num in range(2, n + 1):
+            if self.is_prime(num):
+                print(num)
+    
+    
+    # determines if the number passed as a parameter is prime or not; returns true if the given number is prime, false otherwise
+    def is_prime(self, num):
+        prime = True
+        for x in range(2, num):
+            if num % x == 0:
+                prime = False
+        return prime
 
-# determines if the number passed as a parameter is prime or not; returns true if the given number is prime, false otherwise
-def is_prime(num):
-    prime = True
-    for x in range(2, num):
-        if num % x == 0:
-            prime = False
-    return prime
 
-
-get_primes(get_input())
+main()
